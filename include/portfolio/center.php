4e1a2406628757a9c207c2ca63b6d04b27803912
rwaymouth <rwaymouth@gmail.com>
2013-12-01 13:44:53 -0500

    #2

--- include/portfolio/center.php
+++ include/portfolio/center.php
@@ -27,14 +27,14 @@ if ($wp_query->have_posts()) : while($wp_query->have_posts()) : $wp_query->the_p
 			</buton></a>
 			</div>';
 
-		/*$portflio_single_link = '';
+		$portflio_single_link = '';
 	if ($smof_data['portflio_single_link'] == 1){
-		echo '<div class="portfolio-single-link">';
+		/*echo '<div class="portfolio-single-link">';
 		previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'bmd' ) . ' Previous</span>' );
 		echo '<span class="spacer"></span>';
 		next_post_link( '%link', 'Next <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'bmd' ) . '</span>' );
-		echo '</div></div>';
-		$portflio_single_link = "mt26";*/
+		echo '</div></div>';*/
+		$portflio_single_link = "mt26";
 	}
 			the_content();
 
