55d47bfaa21b82a9366159523f80b52b3a2250ad
rwaymouth <rwaymouth@gmail.com>
2013-12-01 13:40:28 -0500

    Portfolio Fix

--- include/portfolio/center.php
+++ include/portfolio/center.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 global $smof_data, $attachment;
 
-$att_arg = array( 
+$att_arg = array(
 				'post_type'      => 'attachment',
 				'numberposts'    => -1,
 				'post_status'    => null,
@@ -12,7 +12,7 @@ $att_arg = array(
 $attachments = get_posts($att_arg);
 
 
-if ($wp_query->have_posts()) : while($wp_query->have_posts()) : $wp_query->the_post(); 
+if ($wp_query->have_posts()) : while($wp_query->have_posts()) : $wp_query->the_post();
 
 	$project_type = get_post_meta($post->ID, '_bmd_project_type', true);
 
@@ -20,26 +20,25 @@ if ($wp_query->have_posts()) : while($wp_query->have_posts()) : $wp_query->the_p
 
 <div class="portfolio-content">
 
-	<?php 	
-			echo '<div class="portfolio-header"><h1>'.get_the_title().'</h1><nav>';
+	<?php
+			echo '<div class="portfolio-header"><h1>'.get_the_title().'</h1>
+        <a class="portfolio-nav" href="../""><button>
+			Back
+			</buton></a>
+			</div>';
 
-        //wp_nav_menu( array('theme_location' => 'portfolio') );
-			echo "<button> 
-			<a href='../'>Back</a>
-			</buton>	</nav>";
-
-		$portflio_single_link = '';
+		/*$portflio_single_link = '';
 	if ($smof_data['portflio_single_link'] == 1){
 		echo '<div class="portfolio-single-link">';
 		previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'bmd' ) . ' Previous</span>' );
 		echo '<span class="spacer"></span>';
-		next_post_link( '%link', 'Next <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'bmd' ) . '</span>' ); 
+		next_post_link( '%link', 'Next <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'bmd' ) . '</span>' );
 		echo '</div></div>';
-		$portflio_single_link = "mt26";
+		$portflio_single_link = "mt26";*/
 	}
 			the_content();
 
-?>		
+?>
 
 </div><!-- end div .portfolio-content -->
 
