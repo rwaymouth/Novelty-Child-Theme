53579c2ccafeea4776b5baf64deb0d97bd514515
rwaymouth <rwaymouth@gmail.com>
2013-12-01 20:38:58 -0500

    Social Media in Nav

---
+++ include/header/style3.php
@@ -0,0 +1,42 @@
+<?php global $smof_data; ?>
+
+<!-- BEGIN: HEADER -->
+<header id="header" class="style3 clearfix text-center">
+    <div class="container">
+
+        <!-- begin: logo -->
+        <?php
+        if (isset($_COOKIE["pixel_ratio"])) {
+            $pixel_ratio = $_COOKIE["pixel_ratio"];
+            $upload_logo = $pixel_ratio >= 2 ? $smof_data['logo2'] : $smof_data['logo'];
+        } else {
+            $upload_logo = $smof_data['logo'];
+        }
+        ?>
+        <h1 class="logo">
+            <a href="<?php echo home_url() ?>"><img width="<?php echo $smof_data['logo_width'] ?>" src="<?php echo $upload_logo ?>" alt="<?php bloginfo('name') ?>" /></a>
+        </h1>
+        <!-- end: logo -->
+
+
+        <!-- begin: menu -->
+        <?php
+        if (has_nav_menu('main_menu')):
+            $args = array(
+                'theme_location' => 'main_menu',
+                'container'      => 'nav',
+                'menu_id'        => 'menu',
+                'menu_class'     => 'clearfix'
+            );
+            wp_nav_menu($args);
+        endif;
+        ?>
+        <!-- end: menu -->
+        <div class="social-block">
+            <ul class="social">
+                <?php include(locate_template('include/social.php')); ?>
+            </ul>
+        </div>
+    </div><!-- end: div.container  -->
+</header>
+<!-- END: HEADER -->
