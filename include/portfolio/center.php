a06d3dda2bd6d7408503a658f6343f6cedb3d0ed
rwaymouth <rwaymouth@gmail.com>
2013-12-04 15:45:41 -0500

    final fix

--- include/portfolio/center.php
+++ include/portfolio/center.php
@@ -27,11 +27,6 @@ if ($wp_query->have_posts()) : while($wp_query->have_posts()) : $wp_query->the_p
 			</a>
 			</div>
 <?php
-		echo '<div class="portfolio-header clearfix"><h1>'.get_the_title().'</h1>
-        <a class="portfolio-nav" href="../""><button>
-			Back
-			</buton></a>
-			</div>';
 
 		$portflio_single_link = '';
 	if ($smof_data['portflio_single_link'] == 1){
