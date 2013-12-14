c03574e65d75fcf143b14db0314c5c84b1ce8677
rwaymouth <rwaymouth@gmail.com>
2013-12-14 08:53:44 -0500

    Button re-added

--- include/portfolio/center.php
+++ include/portfolio/center.php
@@ -23,7 +23,7 @@ if ($wp_query->have_posts()) : while($wp_query->have_posts()) : $wp_query->the_p
 	<?php
 			echo '<div class="portfolio-header clearfix"><h1>'.get_the_title().'</h1>';?>
         	<a class="portfolio-nav" href="../">
-        		<button>Back</buton>
+        		Back
 			</a>
 			</div>
 <?php
